bd5302368b67d2986b521c12acf725bc40524f65
Alex Smith <alexsmith1@me.com>
2018-02-23 16:43:23 -0500

    q3 and new file structure

---
+++ part2/question3.sql
@@ -0,0 +1,5 @@
+insert into deliveryvehicles values ("H982FKL");
+insert into deliveryvehicles values ("24F 7BH");
+insert into deliveryvehicles values ("BJ0 5FA");
+insert into deliveryvehicles values ("E59 AAP");
+insert into deliveryvehicles values ("FLG9461");
