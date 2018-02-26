016be9f2d51677a46767d0ea5bfc94958206090b
Alex Reiff <alexander.reiff@mail.mcgill.ca>
2018-02-26 13:54:20 -0500

    updated tables in q2 with new schema

--- part2/question2.sql
+++ part2/question2.sql
@@ -1,15 +1,25 @@
-create table items(name text PRIMARY KEY, price real);
+create table items(name text NOT NULL PRIMARY KEY, price real NOT NULL);
 
-create table customers(memberNo int PRIMARY KEY, firstName text lastName text, address text, expiry date);
+create table customers(memberNo int NOT NULL PRIMARY KEY, firstName text NOT NULL, lastName text NOT NULL, address text NOT NULL, expiry date NOT NULL);
 
-create table coupons(couponCode, pName, discountPct int, startDate date, endDate date);
+create table coupons(couponCode int NOT NULL PRIMARY KEY, pName text NOT NULL, discountPct int NOT NULL, startDate date NOT NULL, endDate date NOT NULL, FOREIGN KEY(pName) REFERENCES items(name));
 
-create table orders(orderNo int PRIMARY KEY, total real, toBeDelivered boolean);
+create table employees(employeeID int NOT NULL PRIMARY KEY, firstName text NOT NULL, lastName text NOT NULL);
 
-create table employees(employeeID int PRIMARY KEY, firstName text, lastName text);
+create table managers(employeeID int NOT NULL PRIMARY KEY, FOREIGN KEY(employeeID) REFERENCES employees(employeeID));
 
-create table transactions(transactionID int PRIMARY KEY, amount real, transactionDate date);
+create table cashiers(employeeID int NOT NULL PRIMARY KEY, FOREIGN KEY(employeeID) REFERENCES employees(employeeID));
 
-create table shifts(shiftDate date, employeeID int, wage real, hours real);
+create table deliverydrivers(employeeID int NOT NULL PRIMARY KEY, FOREIGN KEY(employeeID) REFERENCES employees(employeeID));
 
-create table deliveryvehicles(licenseNo char(7) PRIMARY KEY);
+create table orders(orderNo int NOT NULL PRIMARY KEY, memberNo int NOT NULL, total real NOT NULL, toBeDelivered boolean, driverID int, FOREIGN KEY(memberNo) REFERENCES customers(memberNo), FOREIGN KEY(driverID) REFERENCES deliverydrivers(employeeID));
+
+create table ordercontents(orderNo int NOT NULL, pName text NOT NULL, quantity int NOT NULL, FOREIGN KEY(orderNo) REFERENCES orders(orderNo), FOREIGN KEy(pName) REFERENCES items(name), PRIMARY KEY(orderNo, pName));
+
+create table couponRedeems(couponCode int NOT NULL, orderNo int NOT NULL, FOREIGN KEY(couponCode) REFERENCES coupons(couponCode), FOREIGN KEY(orderNo) REFERENCES orders(orderNo), PRIMARY KEY (couponCode, orderNo));
+
+create table transactions(transactionID int NOT NULL PRIMARY KEY, amount real NOT NULL, transactionDate date NOT NULL, cashierID int, FOREIGN KEY(cashierID) REFERENCES cashiers(employeeID));
+
+create table shifts(shiftDate date NOT NULL, employeeID int NOT NULL, wageID int NOT NULL, managerID int NOT NULL, FOREIGN KEY(employeeID) REFERENCES employees(employeeID), FOREIGN KEY(wageID) REFERENCES transactions(transactionID), FOREIGN KEY(managerID) REFERENCES managers(employeeID), PRIMARY KEY(shiftDate, employeeID));
+
+create table deliveryvehicles(licenseNo char(7) NOT NULL PRIMARY KEY, currentdriverID int NOT NULL, FOREIGN KEY(currentDriverID) REFERENCES deliverydrivers(employeeID));
