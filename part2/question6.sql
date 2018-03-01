0e8389a15cbfb30a3faf8a5784c36e7f4d7d4a78
jgalet734 <36783445+jgalet734@users.noreply.github.com>
2018-02-28 20:10:17 -0500

    Update question6.sql

--- part2/question6.sql
+++ part2/question6.sql
@@ -1,4 +1,12 @@
-/* delete all expired coupons from the coupon table and associated redeems*/
+/* extend all coupons that have expired in the last 7 days by an additional 7 days from their original start date  */
 
-DELETE FROM couponredeems where couponcode IN (SELECT couponcode FROM coupons WHERE enddate < current_date);
-DELETE FROM coupons WHERE enddate < current_date;
+UPDATE coupons SET enddate = enddate + 7 WHERE enddate BETWEEN current_date - 7 AND current_date;
+
+/* give a $0.50 wage increase to all cashiers */
+
+UPDATE shifts SET wage = wage + .5 WHERE employeeID IN (SELECT employeeID FROM cashiers);
+
+/* give a $5 instant rebate on any order that is more than $200 */
+
+UPDATE orders SET total = total - 5 WHERE total > 200;
+
