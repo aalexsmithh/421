155d7f0bdd1695904d3a694939ccdd4bb8d1c0f6
jgalet734 <36783445+jgalet734@users.noreply.github.com>
2018-02-28 19:32:08 -0500

    Update question6.sql

--- part2/question6.sql
+++ part2/question6.sql
@@ -1,4 +1,4 @@
-/* delete all expired coupons from the coupon table*/
+/* delete all expired coupons from the coupon table and associated redeems*/
 
 DELETE FROM couponredeems where couponcode IN (SELECT couponcode FROM coupons WHERE enddate < current_date);
 DELETE FROM coupons WHERE enddate < current_date;
