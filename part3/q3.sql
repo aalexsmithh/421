3c6a60c89cb6bdb27d38958486207665da50c80d
jgalet734 <36783445+jgalet734@users.noreply.github.com>
2018-03-24 15:14:27 -0400

    Update q3.sql

--- part3/q3.sql
+++ part3/q3.sql
@@ -2,3 +2,8 @@
 Say for example, all the items that have more than 40% off*/
 
 CREATE INDEX flyer ON coupons (pName, discountpct);
+
+/* This index would be used to calculate how much profit the store made on a given date. It could also be used 
+to calculate profit for a given time of year (range of dates) */
+
+CREATE INDEX profit ON transactions (transactiondate);
