c33f45ab6d5e8786ceae67494f21c413c5344108
jgalet734 <36783445+jgalet734@users.noreply.github.com>
2018-03-24 15:05:05 -0400

    Update q3.sql

--- part3/q3.sql
+++ part3/q3.sql
@@ -1,4 +1,4 @@
 /* This index would be used to create a flyer containing all of the feature items that are currently on sale. 
 Say for example, all the items that have more than 40% off*/
 
-CREATE index flyer ON coupons (pName, discountpct);
+CREATE INDEX flyer ON coupons (pName, discountpct);
