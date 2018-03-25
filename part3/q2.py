76325763f6ff87ba5a09498f2db46441eb675a5c
Justin Bell <justin.bell@mail.mcgill.ca>
2018-03-25 13:45:17 -0400

    Conform to pep8

--- part3/q2.py
+++ part3/q2.py
@@ -1,63 +1,69 @@
 #!/usr/bin/env python2
 
-from pimento import menu
 import time
+
+from pimento import menu
 
 from config import get_config
 
 CONFIG_FNAME = 'config.dev.json'
 
+
 def welcome_msg():
-	print '##############################################'
-	print '\tWELCOME TO GROCERY MASTER 3000'
-	print '##############################################'
-	print '\nLoading ...\n'
-	time.sleep(1)
+    print '##############################################'
+    print '\tWELCOME TO GROCERY MASTER 3000'
+    print '##############################################'
+    print '\nLoading ...\n'
+    time.sleep(1)
+
 
 def main_action():
-	result = menu(
-	['add customer', 'add item', 'add employee', 'add order', 'quit'],
-	pre_prompt = 'What would you like to do:',
-	post_prompt = "Action: ",
-	indexed=True)
-	return result
+    result = menu(
+        ['add customer', 'add item', 'add employee', 'add order', 'quit'],
+        pre_prompt='What would you like to do:',
+        post_prompt="Action: ",
+        indexed=True)
+    return result
+
 
 def add_customer():
-	pass
+    pass
+
 
 def add_item():
-	pass
+    pass
+
 
 def add_employee():
-	pass
+    pass
+
 
 def add_order():
-	pass
+    pass
+
 
 def main():
-	quit = False
-        
-        c = get_config(CONFIG_FNAME)
-        print(c)
+    c = get_config(CONFIG_FNAME)
+    print(c)
 
-	welcome_msg()
+    welcome_msg()
 
-	while 1:
-		action = main_action()
-		if action == 'quit':
-			break
-		elif action == 'add customer':
-			add_customer()
-		elif action == 'add item':
-			add_item()
-		elif action == 'add employee':
-			add_employee()
-		elif actoin == 'add order':
-			add_order()
-		else:
-			# should never happen
-			break
+    while 1:
+        action = main_action()
+        if action == 'quit':
+            break
+        elif action == 'add customer':
+            add_customer()
+        elif action == 'add item':
+            add_item()
+        elif action == 'add employee':
+            add_employee()
+        elif action == 'add order':
+            add_order()
+        else:
+            # should never happen
+            break
 
 
 if __name__ == '__main__':
-	main()
+    main()
